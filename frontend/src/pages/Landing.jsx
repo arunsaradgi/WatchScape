cb2b144d8b639cbc1281afeaffd912cb1f3f62b9
Arun Saradgi <saradgi07@gmail.com>
2023-07-21 10:42:07 +0530

    footer

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,14 +2,16 @@ import { Flex, Heading } from "@chakra-ui/react";
 import React from "react";
 import NavbarLanding from "../components/NavbarLanding";
 import LandingPageContent from "../components/LandingPageContent";
+import Footer from "../components/Footer";
 // import Test from "../components/Test";
 
 export const Landing = () => {
   return (
     <>
-      {/* <Test /> */}
       <NavbarLanding />
       <LandingPageContent />
+      {/* <Test /> */}
+      <Footer />
     </>
   );
 };
